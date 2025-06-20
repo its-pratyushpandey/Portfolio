e4f2b51c67f46446ca6192a07d8a3768396dee28
Pratyush Kumar Pandey <2300030557@kluniversity.in>
2025-06-20 09:00:32 +0530

    main

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -7,7 +7,7 @@ import About from './components/About';
 import Skills from './components/Skills';
 import Projects from './components/Projects';
 import Certificates from './components/Certificates';
-import Contact from './components/Contact';
+import Contact from './components/Contacts';
 
 interface SectionProps {
   children: ReactNode;

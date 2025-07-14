b9976fefc06ab6cc2c5213951c49460b6e3d2df1
Pratyush Kumar Pandey <2300030557@kluniversity.in>
2025-07-14 12:01:28 +0530

    main

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,14 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import postcss from 'postcss';
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
 
 export default defineConfig({
   plugins: [react()],
   css: {
     postcss: {
       plugins: [
-        require('tailwindcss'),
-        require('autoprefixer')
+        tailwindcss,
+        autoprefixer
       ]
     }
   },
